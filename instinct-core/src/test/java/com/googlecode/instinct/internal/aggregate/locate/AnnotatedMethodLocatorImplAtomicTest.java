034a851b4afddb51101a20b840be0030cee4dbe2
Tom Adams <tomjadams@gmail.com>
2007-01-11 06:55:48 +0000

    git-svn-id: http://instinct.googlecode.com/svn/trunk@102 b4a9d0b3-2925-0410-989b-71e32af4dc30

--- instinct-core/src/test/java/com/googlecode/instinct/internal/aggregate/locate/AnnotatedMethodLocatorImplAtomicTest.java
+++ instinct-core/src/test/java/com/googlecode/instinct/internal/aggregate/locate/AnnotatedMethodLocatorImplAtomicTest.java
@@ -1,10 +1,25 @@
 package com.googlecode.instinct.internal.aggregate.locate;
 
+import java.lang.reflect.Method;
+import com.googlecode.instinct.internal.util.Suggest;
 import com.googlecode.instinct.test.InstinctTestCase;
-import com.googlecode.instinct.test.checker.ClassChecker;
+import static com.googlecode.instinct.test.checker.ClassChecker.checkClass;
 
 public final class AnnotatedMethodLocatorImplAtomicTest extends InstinctTestCase {
+    private static final Class<?> CLASS_WITH_NO_ANNOTATIONS = Object.class;
+    private AnnotatedMethodLocator locator;
+
     public void testProperties() {
-        ClassChecker.checkClass();
+        checkClass(AnnotatedMethodLocatorImpl.class, AnnotatedMethodLocator.class);
+    }
+
+    public void testLocateOnAClassWithNoAnnotationsGiveNoMethod() {
+        final Method[] methods = locator.locate(CLASS_WITH_NO_ANNOTATIONS, Suggest.class);
+        assertEquals(0, methods.length);
+    }
+
+    @Override
+    public void setUpSubject() {
+        locator = new AnnotatedMethodLocatorImpl();
     }
 }
