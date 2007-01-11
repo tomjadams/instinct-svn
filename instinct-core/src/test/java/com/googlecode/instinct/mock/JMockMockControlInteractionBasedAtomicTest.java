8f7a7f7a1f462f132c7403bb17d0e24ad1dd1f78
Tom Adams <tomjadams@gmail.com>
2007-01-11 02:00:17 +0000

    git-svn-id: http://instinct.googlecode.com/svn/trunk@92 b4a9d0b3-2925-0410-989b-71e32af4dc30

--- instinct-core/src/test/java/com/googlecode/instinct/mock/JMockMockControlInteractionBasedAtomicTest.java
+++ instinct-core/src/test/java/com/googlecode/instinct/mock/JMockMockControlInteractionBasedAtomicTest.java
@@ -1,14 +1,9 @@
 package com.googlecode.instinct.mock;
 
 import com.googlecode.instinct.test.InstinctTestCase;
-import junit.framework.AssertionFailedError;
-import org.jmock.Mock;
-import org.jmock.core.matcher.InvokeOnceMatcher;
 
 @SuppressWarnings({"ErrorNotRethrown"})
 public final class JMockMockControlInteractionBasedAtomicTest extends InstinctTestCase {
-
-
     public void testGetMockedObject() {
     }
 
@@ -16,28 +11,8 @@ public final class JMockMockControlInteractionBasedAtomicTest extends InstinctTe
     }
 
     public void testVerify() {
-        try {
-            final Mock mock = createMock();
-            mock.expects(new InvokeOnceMatcher()).method("read");
-            createControl(mock).verify();
-            fail();
-        } catch (AssertionFailedError expected) {
-        }
     }
 
     public void testToString() {
-        assertEquals("mockReadable", createControl().toString());
-    }
-
-    private MockControl createControl() {
-        return createControl(createMock());
-    }
-
-    private MockControl createControl(final Mock mock) {
-        return new JMockMockControl(mock);
-    }
-
-    private Mock createMock() {
-        return new Mock(Readable.class);
     }
 }
