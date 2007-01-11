f6f6fa29a5ca698230ade25f18b72fd394714db3
Tom Adams <tomjadams@gmail.com>
2007-01-11 06:26:49 +0000

    git-svn-id: http://instinct.googlecode.com/svn/trunk@96 b4a9d0b3-2925-0410-989b-71e32af4dc30

--- instinct-core/src/test/java/com/googlecode/instinct/test/InstinctTestCase.java
+++ instinct-core/src/test/java/com/googlecode/instinct/test/InstinctTestCase.java
@@ -1,13 +1,25 @@
 package com.googlecode.instinct.test;
 
+import static com.googlecode.instinct.mock.Mocker.verify;
 import junit.framework.TestCase;
 
 @SuppressWarnings("NoopMethodInAbstractClass")
 public abstract class InstinctTestCase extends TestCase {
+    @SuppressWarnings({"ProhibitedExceptionDeclared"})
     @Override
-    public final void setUp() {
+    public void runBare() throws Throwable {
         setUpMocks();
         setUpSubject();
+        try {
+            runTest();
+            verify();
+        } finally {
+            tearDown();
+        }
+    }
+
+    @Override
+    public final void setUp() {
     }
 
     public void setUpMocks() {
