af641212bc12c5feee5b136d2e71c7bf55404d84
Tom Adams <tomjadams@gmail.com>
2007-01-11 06:48:48 +0000

    git-svn-id: http://instinct.googlecode.com/svn/trunk@100 b4a9d0b3-2925-0410-989b-71e32af4dc30

--- instinct-core/src/main/java/com/googlecode/instinct/internal/aggregate/locate/AnnotatedClassLocator.java
+++
@@ -1,9 +0,0 @@
-package com.googlecode.instinct.internal.aggregate.locate;
-
-import java.io.File;
-import java.io.FileFilter;
-import com.googlecode.instinct.internal.util.JavaClassName;
-
-public interface AnnotatedClassLocator {
-    JavaClassName[] locate(File root, final FileFilter filter);
-}
