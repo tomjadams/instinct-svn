1e89cf735eb6ae56e905a39709a31f1dd6e582a2
Tom Adams <tomjadams@gmail.com>
2007-01-11 06:58:58 +0000

    git-svn-id: http://instinct.googlecode.com/svn/trunk@103 b4a9d0b3-2925-0410-989b-71e32af4dc30

--- instinct-core/src/test/java/com/googlecode/instinct/internal/aggregate/locate/AnnotatedMethodLocatorImplAtomicTest.java
+++ instinct-core/src/test/java/com/googlecode/instinct/internal/aggregate/locate/AnnotatedMethodLocatorImplAtomicTest.java
@@ -1,12 +1,11 @@
 package com.googlecode.instinct.internal.aggregate.locate;
 
 import java.lang.reflect.Method;
-import com.googlecode.instinct.internal.util.Suggest;
 import com.googlecode.instinct.test.InstinctTestCase;
 import static com.googlecode.instinct.test.checker.ClassChecker.checkClass;
 
 public final class AnnotatedMethodLocatorImplAtomicTest extends InstinctTestCase {
-    private static final Class<?> CLASS_WITH_NO_ANNOTATIONS = Object.class;
+    private static final Class<?> CLASS_WITH_NO_ANNOTATED_METHODS = Object.class;
     private AnnotatedMethodLocator locator;
 
     public void testProperties() {
@@ -14,12 +13,34 @@ public final class AnnotatedMethodLocatorImplAtomicTest extends InstinctTestCase
     }
 
     public void testLocateOnAClassWithNoAnnotationsGiveNoMethod() {
-        final Method[] methods = locator.locate(CLASS_WITH_NO_ANNOTATIONS, Suggest.class);
+        final Method[] methods = locator.locate(WithoutAnnotatedMethods.class, Override.class);
+        assertEquals(0, methods.length);
+    }
+
+    public void testLocateOnAClassWithSeveralAnnotationsGiveSeveralMethod() {
+        final Method[] methods = locator.locate(WithAnnotatedMethods.class, Override.class);
         assertEquals(0, methods.length);
     }
 
     @Override
     public void setUpSubject() {
         locator = new AnnotatedMethodLocatorImpl();
+    }
+
+    @SuppressWarnings({"EmptyClass"})
+    private static class WithoutAnnotatedMethods {
+    }
+
+    private static class WithAnnotatedMethods {
+
+        @Override
+        public String toString() {
+            return super.toString();
+        }
+
+        @Override
+        public boolean equals(final Object obj) {
+            return super.equals(obj);
+        }
     }
 }
