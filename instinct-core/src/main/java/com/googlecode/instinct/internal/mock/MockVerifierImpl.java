b207f8afd8907df379053edd024a416565490d3e
Tom Adams <tomjadams@gmail.com>
2007-01-12 04:25:10 +0000

    git-svn-id: http://instinct.googlecode.com/svn/trunk@120 b4a9d0b3-2925-0410-989b-71e32af4dc30

--- instinct-core/src/main/java/com/googlecode/instinct/internal/mock/MockVerifierImpl.java
+++ instinct-core/src/main/java/com/googlecode/instinct/internal/mock/MockVerifierImpl.java
@@ -2,8 +2,8 @@ package com.googlecode.instinct.internal.mock;
 
 import com.googlecode.instinct.internal.util.Suggest;
 
+@Suggest("Do we need this anymore???")
 public final class MockVerifierImpl implements MockVerifier {
-
     @Suggest("Implement...")
     public void verify(final Object instance) {
     }
