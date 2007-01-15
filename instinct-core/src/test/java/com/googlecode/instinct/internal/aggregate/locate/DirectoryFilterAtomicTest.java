f015a25c59d2fbce920260b82f6d5794e0d685c6
Tom Adams <tomjadams@gmail.com>
2007-01-15 00:22:30 +0000

    git-svn-id: http://instinct.googlecode.com/svn/trunk@138 b4a9d0b3-2925-0410-989b-71e32af4dc30

--- instinct-core/src/test/java/com/googlecode/instinct/internal/aggregate/locate/DirectoryFilterAtomicTest.java
+++ instinct-core/src/test/java/com/googlecode/instinct/internal/aggregate/locate/DirectoryFilterAtomicTest.java
@@ -1,11 +1,26 @@
 package com.googlecode.instinct.internal.aggregate.locate;
 
+import java.io.File;
 import java.io.FileFilter;
+import static com.googlecode.instinct.mock.Mocker.expects;
+import static com.googlecode.instinct.mock.Mocker.mock;
+import static com.googlecode.instinct.mock.Mocker.returnValue;
 import com.googlecode.instinct.test.InstinctTestCase;
 import static com.googlecode.instinct.test.checker.ClassChecker.checkClass;
 
 public final class DirectoryFilterAtomicTest extends InstinctTestCase {
     public void testProperties() {
         checkClass(DirectoryFilter.class, FileFilter.class);
+    }
+
+    public void testAccept() {
+        checkAccept(true);
+        checkAccept(false);
+    }
+
+    private void checkAccept(final boolean value) {
+        final File pathname = mock(File.class);
+        expects(pathname).method("isDirectory").will(returnValue(value));
+        assertEquals(value, new DirectoryFilter().accept(pathname));
     }
 }
