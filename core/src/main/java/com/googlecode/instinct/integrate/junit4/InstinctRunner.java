3f7e4754f1da05d426e3b8f394f17522757b17de
Chris Myers <cwmyers@gmail.com>
2007-08-13 06:52:03 +0000

    git-svn-id: http://instinct.googlecode.com/svn/trunk@585 b4a9d0b3-2925-0410-989b-71e32af4dc30

--- core/src/main/java/com/googlecode/instinct/integrate/junit4/InstinctRunner.java
+++ core/src/main/java/com/googlecode/instinct/integrate/junit4/InstinctRunner.java
@@ -16,74 +16,36 @@
 
 package com.googlecode.instinct.integrate.junit4;
 
-import com.googlecode.instinct.internal.core.ContextClass;
-import com.googlecode.instinct.internal.core.ContextClassImpl;
 import com.googlecode.instinct.internal.core.SpecificationMethod;
-import com.googlecode.instinct.internal.runner.SpecificationResult;
-import com.googlecode.instinct.internal.util.ExceptionFinder;
-import com.googlecode.instinct.internal.util.ExceptionFinderImpl;
-import com.googlecode.instinct.internal.util.Suggest;
-import com.googlecode.instinct.marker.annotate.ContextClasses;
+import com.googlecode.instinct.internal.util.ObjectFactory;
+import com.googlecode.instinct.internal.util.ObjectFactoryImpl;
+import static com.googlecode.instinct.internal.util.ParamChecker.checkNotNull;
 import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
 import org.junit.runner.Description;
+import static org.junit.runner.Description.createSuiteDescription;
 import org.junit.runner.Runner;
-import org.junit.runner.notification.Failure;
 import org.junit.runner.notification.RunNotifier;
 
-@Suggest("Test this.")
 public final class InstinctRunner extends Runner {
-    private final ExceptionFinder exceptionFinder = new ExceptionFinderImpl();
+    private final ObjectFactory objectFactory = new ObjectFactoryImpl();
+    private final SpecificationMethodBuilder specificationMethodBuilder = new SpecificationMethodBuilderImpl();
     private final Class<?> classToRun;
 
     public InstinctRunner(final Class<?> classToRun) {
+        checkNotNull(classToRun);
         this.classToRun = classToRun;
     }
 
     @Override
     public Description getDescription() {
-        return Description.createSuiteDescription(classToRun);
+        return createSuiteDescription(classToRun);
     }
 
     @Override
     public void run(final RunNotifier notifier) {
-        final Set<SpecificationMethod> methods = createSpecifications(getAnnotatedClasses(classToRun));
-        for (final SpecificationMethod specificationMethod : methods) {
-            setUpAndRunSpecification(notifier, specificationMethod);
-        }
-    }
-
-    private void setUpAndRunSpecification(final RunNotifier notifier, final SpecificationMethod specificationMethod) {
-        final Description description = Description.createTestDescription(specificationMethod.getSpecificationMethod().getDeclaringClass(),
-                specificationMethod.getName());
-        notifier.fireTestStarted(description);
-        final SpecificationResult specificationResult = specificationMethod.run();
-        if (specificationResult.completedSuccessfully()) {
-            notifier.fireTestFinished(description);
-        } else {
-            notifier.fireTestFailure(createFailure(description, specificationResult));
-        }
-    }
-
-    private Failure createFailure(final Description description, final SpecificationResult specificationResult) {
-        final Throwable rootCause = exceptionFinder.getRootCause((Throwable) specificationResult.getStatus().getDetailedStatus());
-        return new Failure(description, rootCause);
-    }
-
-    private Set<SpecificationMethod> createSpecifications(final Class<?>[] annotatedClasses) {
-        final Set<SpecificationMethod> methods = new HashSet<SpecificationMethod>();
-        for (final Class<?> annotatedClass : annotatedClasses) {
-            final ContextClass contextClass = new ContextClassImpl(annotatedClass);
-            final Collection<SpecificationMethod> specificationMethods = contextClass.buildSpecificationMethods();
-            methods.addAll(specificationMethods);
-        }
-        return methods;
-    }
-
-    @Suggest({"What if annotation not present?", "What if annotation is empty?"})
-    private static Class<?>[] getAnnotatedClasses(final Class<?> cls) {
-        final ContextClasses specificationClass = cls.getAnnotation(ContextClasses.class);
-        return specificationClass.value();
+        checkNotNull(notifier);
+        final Collection<SpecificationMethod> specificationMethods = specificationMethodBuilder.build(classToRun);
+        final SpecificationRunner specificationRunner = objectFactory.create(SpecificationRunnerImpl.class, notifier);
+        specificationRunner.run(specificationMethods);
     }
 }
---
+++ core/src/test/java/com/googlecode/instinct/integrate/junit4/InstinctRunnerAtomicTest.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2006-2007 Workingmouse
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.googlecode.instinct.integrate.junit4;
+
+import static com.googlecode.instinct.expect.Mocker12.eq;
+import static com.googlecode.instinct.expect.Mocker12.expects;
+import static com.googlecode.instinct.expect.Mocker12.mock;
+import static com.googlecode.instinct.expect.Mocker12.returnValue;
+import com.googlecode.instinct.internal.core.SpecificationMethod;
+import com.googlecode.instinct.internal.util.ObjectFactory;
+import com.googlecode.instinct.test.InstinctTestCase;
+import static com.googlecode.instinct.test.checker.ClassChecker.checkClass;
+import static com.googlecode.instinct.test.reflect.SubjectCreator.createSubjectWithConstructorArgs;
+import java.util.Collection;
+import org.junit.runner.Runner;
+import org.junit.runner.notification.RunNotifier;
+
+@SuppressWarnings({"unchecked"})
+public final class InstinctRunnerAtomicTest extends InstinctTestCase {
+    private static final Class<?> CLASS_TO_RUN = String.class;
+    private Runner runner;
+    private RunNotifier runNotifier;
+    private SpecificationMethodBuilder specificationMethodBuilder;
+    private Collection<SpecificationMethod> specificationMethods;
+    private SpecificationRunner specificationRunner;
+    private ObjectFactory objectFactory;
+
+    @Override
+    public void setUpTestDoubles() {
+        runNotifier = mock(RunNotifier.class);
+        specificationMethodBuilder = mock(SpecificationMethodBuilder.class);
+        specificationMethods = mock(Collection.class);
+        specificationRunner = mock(SpecificationRunner.class);
+        objectFactory = mock(ObjectFactory.class);
+    }
+
+    @Override
+    public void setUpSubject() {
+        runner = createSubjectWithConstructorArgs(InstinctRunner.class, new Object[]{CLASS_TO_RUN}, specificationMethodBuilder, objectFactory);
+    }
+
+    public void testConformsToClassTraits() {
+        checkClass(InstinctRunner.class);
+    }
+
+    public void testRunsSuitesContainingContextClasses() {
+        expects(specificationMethodBuilder).method("build").with(eq(CLASS_TO_RUN)).will(returnValue(specificationMethods));
+        expects(objectFactory).method("create").with(eq(SpecificationRunnerImpl.class), eq(new Object[]{runNotifier})).will(returnValue(specificationRunner));
+        expects(specificationRunner).method("run").with(eq(specificationMethods));
+        runner.run(runNotifier);
+    }
+}
