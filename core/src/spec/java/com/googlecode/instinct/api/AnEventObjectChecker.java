34ac74f05b8f896785561044b8afea3ffc0fb9a1
Tom Adams <tomjadams@gmail.com>
2007-12-11 23:21:47 +0000

    git-svn-id: http://instinct.googlecode.com/svn/trunk@993 b4a9d0b3-2925-0410-989b-71e32af4dc30

--- core/src/spec/java/com/googlecode/instinct/api/AnEventObjectChecker.java
+++ core/src/spec/java/com/googlecode/instinct/api/AnEventObjectChecker.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2006-2007 Workingmouse
+ * Copyright 2006-2007 Tom Adams
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -19,27 +19,31 @@ package com.googlecode.instinct.api;
 import com.googlecode.instinct.expect.state.checker.EventObjectChecker;
 import com.googlecode.instinct.expect.state.checker.EventObjectCheckerImpl;
 import com.googlecode.instinct.integrate.junit4.InstinctRunner;
-import com.googlecode.instinct.marker.annotate.Context;
+import com.googlecode.instinct.marker.annotate.BeforeSpecification;
 import com.googlecode.instinct.marker.annotate.Specification;
+import com.googlecode.instinct.marker.annotate.Subject;
 import java.util.EventObject;
 import org.junit.runner.RunWith;
 
 @RunWith(InstinctRunner.class)
-@Context
-public class AnEventObjectChecker {
+public final class AnEventObjectChecker {
+    @Subject(auto = false) private EventObjectChecker<EventObject> checker;
+
+    @BeforeSpecification
+    public void before() {
+        checker = new EventObjectCheckerImpl<EventObject>(new ASpecificationEvent(this));
+    }
 
     @Specification
     public void shouldHandleEvents() {
-        final EventObject eventObject = new ASpecificationEvent(this);
-        final EventObjectChecker<EventObject> objectChecker = new EventObjectCheckerImpl<EventObject>(eventObject);
-
-        objectChecker.isAnEventFrom(this);
-        objectChecker.isAnEventFrom(EventObject.class, this);
-        objectChecker.isAnEventFrom(ASpecificationEvent.class, this);
-        objectChecker.isNotAnEventFrom(AnotherSpecificationEvent.class, this);
+        checker.isAnEventFrom(this);
+        checker.isAnEventFrom(EventObject.class, this);
+        checker.isAnEventFrom(ASpecificationEvent.class, this);
+        checker.isNotAnEventFrom(AnotherSpecificationEvent.class, this);
     }
 
-    private class ASpecificationEvent extends EventObject {
+    private final class ASpecificationEvent extends EventObject {
+        private static final long serialVersionUID = 4231685353656364481L;
 
         public ASpecificationEvent(final Object source) {
             super(source);
@@ -47,6 +51,7 @@ public class AnEventObjectChecker {
     }
 
     private class AnotherSpecificationEvent extends EventObject {
+        private static final long serialVersionUID = -5268419544552483659L;
 
         public AnotherSpecificationEvent(final Object source) {
             super(source);
