2904ee065a58971619c18bee5ca7561c891da402
Tom Adams <tomjadams@gmail.com>
2008-07-24 23:54:54 +0000

    Adding in ability to turn on & off naming conventions.
    git-svn-id: http://instinct.googlecode.com/svn/trunk@1365 b4a9d0b3-2925-0410-989b-71e32af4dc30

--- core/src/main/java/com/googlecode/instinct/marker/annotate/Context.java
+++ core/src/main/java/com/googlecode/instinct/marker/annotate/Context.java
@@ -16,6 +16,8 @@
 
 package com.googlecode.instinct.marker.annotate;
 
+import com.googlecode.instinct.marker.naming.NamingConvention;
+import com.googlecode.instinct.marker.naming.SpecificationNamingConvention;
 import java.lang.annotation.Documented;
 import static java.lang.annotation.ElementType.TYPE;
 import java.lang.annotation.Inherited;
@@ -33,12 +35,13 @@ import java.lang.annotation.Target;
 @Target({TYPE})
 public @interface Context {
     /**
-     * The group(s) the specifications in this context belong to.
-     * Setting this attribute will cause all specifications in the context to be marked as belonging to the specified group. In this way using this
-     * attribute is a shorthand method of marking every individual specification.
-     *
+     * The group(s) the specifications in this context belong to. Setting this attribute will cause all specifications in the context to be marked as
+     * belonging to the specified group. In this way using this attribute is a shorthand method of marking every individual specification.
      * @return The group(s) the specifications in this context belong to.
      * @see Specification#groups()
      */
     String[] groups() default "ALL";
+
+    /** @return  */
+    Class<? extends NamingConvention> specificationNamingConvention() default SpecificationNamingConvention.class;
 }
